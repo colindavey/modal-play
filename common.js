5f4bd704e69d66a3c695380a9208d4edcb39a031
Colin Davey <himself@colindavey.com>
2021-07-25 14:22:25 -0700

    Pretty much have it working

--- common.js
+++ common.js
@@ -11,23 +11,3 @@ const BLACK_ROOK = '\u265C'
 const BLACK_BISHOP = '\u265D'
 const BLACK_KNIGHT = '\u265E'
 // const BLACK_PAWN = '\u265F'
-
-const showChoice = value => {
-    console.log('show', value)
-    document.getElementById("choice").innerHTML = value ? value : 'canceled'
-}
-
-const setPromoteButtons = (qBtn, rBtn, bBtn, nBtn, cancelBtn, color) => {
-    if (color === 'w') {
-        qBtn.innerHTML = WHITE_QUEEN;
-        rBtn.innerHTML = WHITE_ROOK;
-        bBtn.innerHTML = WHITE_BISHOP;
-        nBtn.innerHTML = WHITE_KNIGHT;    
-    } else {
-        qBtn.innerHTML = BLACK_QUEEN;
-        rBtn.innerHTML = BLACK_ROOK;
-        bBtn.innerHTML = BLACK_BISHOP;
-        nBtn.innerHTML = BLACK_KNIGHT;    
-    }
-    cancelBtn.innerHTML = 'Cancel';
-}
